30d8bec2775cec3890892ff3ad7bb18815e47a0b
Vishnu Vardhan Maheswaran <vishnu191299@gmail.com>
2024-08-15 20:45:56 -0700

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Count Digits/count-digits.py
@@ -0,0 +1,31 @@
+#User function Template for python3
+
+
+class Solution:
+    def evenlyDivides (self, N):
+        # code here
+        a=str(N)
+        count=0
+        for i in a:
+            if int(i)==0:
+                continue
+            elif N%int(i)==0:
+                count+=1
+        return count
+                
+            
+
+
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+if __name__ == '__main__': 
+    t = int (input ())
+    for _ in range (t):
+        N = int(input())
+       
+
+        ob = Solution()
+        print(ob.evenlyDivides(N))
+# } Driver Code Ends
