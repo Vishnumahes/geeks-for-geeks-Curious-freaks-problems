4f1408e4c338ccb569490d9e0ed4248e39fa0681
Vishnu Vardhan Maheswaran <vishnu191299@gmail.com>
2024-08-22 22:21:45 -0700

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Rotate Array by One/rotate-array-by-one.py
@@ -0,0 +1,28 @@
+#User function Template for python3
+
+class Solution:
+    def rotate(self, arr):
+        last_element = arr[-1]
+        for i in range(len(arr)-1,0,-1):
+            arr[i] = arr[i-1]
+        arr[0] = last_element
+        return arr
+        
+    
+
+
+
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+if __name__ == "__main__":
+    t = int(input())
+    while t > 0:
+        arr = list(map(int, input().split()))
+        ob = Solution()
+        ob.rotate(arr)
+        print(" ".join(map(str, arr)))
+        t -= 1
+
+# } Driver Code Ends
